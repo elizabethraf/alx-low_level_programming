b8696a5e46712f4151d4d7b4967ce2275b1acefe
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-07 12:16:26 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
+#include "main.h"
+
 /**
- * * main - Entry point
- * * check is the rand n is negative or positive,
- * * or zero and print accordingly
- * * Return: Always (0) (Success)
- * **/
-#include <stdio>
+ * main - Entry point
+ *  check is the rand n is negative or positive,
+ *  or zero and print accordingly
+ *  Return: Always (0) (Success)
+ **/
 
 int main(void)
 {
-	int ch
-		;
-	for (ch = 'A'; ch <= 'Z'; ch++)
-		_putchar(ch);
+	int ch;
+	char myput[] = "_putchar";
+	int myLen = strlen(myput);
+	for ( int i = 0; i < len; i++)
+	{
+		ch = (int)myput[i]
+		_putchar(myput[i]
+	}
 	_putchar('\n');
 	return (0);
 }
