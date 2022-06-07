bfef1db416ca6cfba258fc6d5b7d27f5085de242
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-07 09:52:45 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+ * * main - Entry point
+ * * check is the rand n is negative or positive,
+ * * or zero and print accordingly
+ * * Return: Always (0) (Success)
+ * **/
+#include <stdio>
+
+int main(void)
+{
+	int ch
+		;
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+	return (0);
+}
