7e7e16a11dbe137987f4853cdddbf120d1254492
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-06 20:33:20 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+/**
+* main - Entry point
+* check is the rand n is negative or positive,
+* or zero and print accordingly
+* Return: Always (0) (Success)
+**/
+#include<stdio.h>
+int main(void)
+{
+	in ch;
+	for(ch = 'a'; <= 'z'; ch++)
+		putchar(ch);
+	putchar('/n');
+
+	for (ch= 'A'; ch<= 'Z'; ch++)
+		putchar(ch);
+	putchar ('\n');
+	getch();
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+/**
+* main - Entry point
+* check is the rand n is negative or positive,
+* or zero and print accordingly
+* Return: Always (0) (Success)
+**/
+
+#include <stdio.h>
+ 
+int main (void) {
+
+   int a = 10;
+
+   while( a < 20 ) {
+      printf("value of a: %d\n", a);
+      a++;
+      printf('\n');
+   }
+ 
+   return (0);
