9ecb2ec13a594b4fa64a4980f04838491ec04c38
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-06 17:41:04 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-
+/**
+* main - Entry point
+* check is the rand n is negative or positive,
+* or zero and print accordingly
+* Return: Always (0) (Success)
+**/
 int main(void)
 {
 
@@ -7,7 +12,7 @@ int ch;
 
 for (ch = 'a'; ch <= 'z'; ch++)
 putchar (ch);
-ch = putchar();
+putchar('\n');
 return (0);
 
 }
