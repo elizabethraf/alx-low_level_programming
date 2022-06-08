f97ed35e1376e4acf78befb3f50ace68784b4bfc
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-08 09:53:35 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* _isLower - checks for lower
+* then return 1 or 0
+* Makes sure we print using putchar
+**/
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	}     
+	else
+	{
+		return (0);
+	}
+
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,12 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 #include <stdio.h>
+#include <ctype.h>
 
 int _putchar(int chr);
 void print_puts(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif
