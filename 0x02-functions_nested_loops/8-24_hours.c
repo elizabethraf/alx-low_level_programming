264965c85f9a887bb54fc159bb33bfa442241141
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-08 18:48:43 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* jack_bauer - print text
+* @h,m: return
+* h = hour, m = minutes
+* Returning:you can listen to this soundtrack 24 hours
+* Loop breaks before 00:00
+* Makes sure we print using putchar
+**/
+void jack_bauer(void)
+{
+	int h, m;
+
+for ('h =0; h < 24; h++')
+
+{
+
+for (m = 0; m < 60; m++)
+
+	{
+	_putchar((h % 10) + '0');
+	_putchar((h / 10) + '0');
+	_putchar((m % 10) + '0');
+	_putchar((m / 10) + '0');
+	_putchar(';');
+	}
+	return (0);
+	}
+}
