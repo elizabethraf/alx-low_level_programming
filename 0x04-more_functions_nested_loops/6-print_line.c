07797df4e532986e56dbf08542cf6ad3236de3eb
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 13:15:44 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+void print_line(int n)
+{
+	int ln;
+	if (n > 0)
+	{
+		for (ln = 0; ln < n; kn++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
