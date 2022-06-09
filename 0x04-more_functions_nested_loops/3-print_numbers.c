0d8cd9134e8b231ab3a88cddc1ba43cdb459ad35
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 11:57:42 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_numbers - Entry point
+* @c:
+* Print with _putchar
+* return funtion that print 0 - 9
+* Return: 0 aslways if (successful)
+**/
+void print_numbers(void)
+{
+	int x;
+	for (x = 0; x <= 9; x++)
+		{
+			_putchar('0'+ x);
+		}
+	_putchar ('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
