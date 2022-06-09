37baa3f75b9c39d6ff3c63717e4444cfbd3123c9
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 09:32:41 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isdigit - Entry point
+* @c:
+* Print with _putchar
+* Return: 1 if c is a digit and 0 otherwise
+**/
+int _isdigit(int c)
+{
+	if (isdigit(c))
+	{
+
+	return (0);
+
+	}
+	else
+	{
+
+	return (1);
+
+	}
+}
