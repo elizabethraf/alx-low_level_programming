7bce4e3ce3f62f1fcedcc653fd88698e49b99a4b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 10:34:15 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - Entry point
+* @a: recieves two integers
+* @b: recieves two integers
+* Print with _putchar
+* Return: 0 always if (succesful)
+**/
+int mul(int a, int b)
+{
+	return (a * b);
+}
