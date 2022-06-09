81a5a87d08b79f68c05bc5c89b89f681429db25d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 12:27:37 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,15 @@ void print_most_numbers(void)
 	int k;
 	for (k = 0; k <= 9; k++)
 	{
-		if (k != 2 || k != 4)
+		if (k == 2)
+		{
+			/* _putchar('0' +  k);*/
+		}
+		else if (k == 4)
+		{
+			/* _putchar('0' +  k);*/
+		}
+		else
 		{
 			_putchar('0' +  k);
 		}
