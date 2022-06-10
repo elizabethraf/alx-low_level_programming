c9a3ba2d705c4ef4b784a49edf8ebd53afbf48f0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 17:52:07 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,20 @@
 #include "main.h"
-
-
+/**
+* print_line - Entry point
+* Print with _putchar
+*
+* return  0 always if (successful)
+* @n: print the number of characters
+**/
 void print_line(int n)
 {
 	int ln;
+
 	if (n > 0)
+
 	{
 		for (ln = 0; ln < n; ln++)
+
 		{
 			_putchar('_');
 		}
