0b0c47b4b2fbac4fb81f269f97e369c5f2b40f7f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 18:00:27 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,24 @@
 #include "main.h"
-
+/**
+* print_diagonal - Entry point
+* Print with _putchar
+* @n: number of times character to be printed
+* Return: 0 aslways if (successful)
+**/
 void print_diagonal(int n)
 {
 	int ln;
+
 	if (n > 0)
+
 	{
 		for (ln = 0; ln < n; ln++)
+
 		{
 			_putchar(' ');
+
 			if (ln == (n - 1))
+
 			{
 			_putchar('\\');
 			}
