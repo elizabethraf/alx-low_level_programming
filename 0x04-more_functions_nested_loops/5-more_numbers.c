e5545c06484eb48f0b5cc228be1dc9cddda56e7e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 17:30:07 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,31 @@
 #include "main.h"
-
+/**
+* more_numbers - Entry point
+* Print with _putchar
+*
+* return funtion that print 0 - 14
+* Return: 0 aslways if (successful)
+*
+**/
 void more_numbers(void)
 {
 	int c, k;
 
 	for (k = 0; k < 10; k++)
+
 	{
 		for (c = 0; c <= 14; c++)
+
 		{
 			if (c >= 10)
+
 			{
-				_putchar('0' + c/10);
+				_putchar ('0' + c / 10);
 			}
-			_putchar('0' + c%10);
+
+			_putchar ('0' + c % 10);
 		}
+
 		_putchar('\n');
 	}
 }
