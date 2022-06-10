ef85cc68ceb209efb0baff737ff39b935e2fccc8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-09 17:00:16 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,20 +1,32 @@
 #include "main.h"
-
-
+/**
+* print_most_numbers - Entry point
+* Print with _putchar
+*
+* return do not print 2 and 4
+* Return: 0 aslways if (successful)
+**/
 void print_most_numbers(void)
 {
 	int k;
+
 	for (k = 0; k <= 9; k++)
+
 	{
 		if (k == 2)
+
 		{
 			/* skipp */
 		}
+
 		else if (k == 4)
+
 		{
 			/* skipp */
 		}
+
 		else
+
 		{
 			_putchar('0' +  k);
 		}
