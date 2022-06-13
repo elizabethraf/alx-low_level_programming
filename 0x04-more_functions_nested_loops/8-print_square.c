81e9a47b61655e87eb94df3bdaf8c3964dd8b53e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-13 16:11:43 -0700

    9-strcpy.c f

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+/**
+* print_square - Entry point
+* Print with _putchar
+*
+* return if 0 print only new line
+* Return: 0 always if (successful)
+**/
+void print_square(int size)
+{
+	int wid, hgt;
+
+	 size < 0
+
+	{
+	if (hgt = 0; wid = 0; size++)
+		for (wid = 0; wid < size; wid++)
+
+		_putchar(#);
+		{
+		if (wid == 0 (hgt == size - 1)
+			continue;
+		}
+			_putchar('\n');
+		}
+}
