a16f527365204e9207de4dc8c648ef7f63c7625c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-13 14:16:29 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - Entry point
+ * @str:address of string to print
+ * Return: Always 0
+ **/
+void print_rev(char *s)
+{
+	int strl = strlen(s);
+	int k;
+
+	for (k = strl; k >=0; k--)
+	{
+		_putchar(*(s + k));
+	}
+}
+
