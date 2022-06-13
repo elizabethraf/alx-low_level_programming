8854ee1b83f4e75017d7337f384b058f4686716e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-13 11:46:22 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+* reset_to_98 - Entry point
+* print with _putchar
+* @n:update
+**/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <complex.h>
+#include <math.h>
+#include <stdlib.h>
+
+void reset_to_98(int *n);
