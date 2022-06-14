9c7aee0beafc3e3631ae3b5f621c83b5d78e4a9f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-13 17:12:19 -0700

    print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * _puts - Entry point
- * @str:address of string to print
+ * print_array - Entry point
+ * @n:function that prints n
+ * @a:function that print
  * Return: Always 0
  **/
 
@@ -9,9 +10,9 @@ void print_array(int *a, int n)
 {
 	int k;
 
-	for (k=0; k < n; k++)
+	for (k = 0; k < n; k++)
 	{
-		printf("%d, ", *(a+k));
+		printf("%d, ", *(a + k));
 	}
 	printf("\n");
 }
