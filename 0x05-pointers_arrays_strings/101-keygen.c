db5dcc0a7e2f644ef073e05079996d0d763f62ae
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-14 20:52:14 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,15 +1,45 @@
 #include "main.h"
+
+using namespace std;
+static cont char alphnum[] = "0123456" "!@$%*"
+"ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
+int strLen = sizeof(alphnum)-1;
+char GenRand()
+{
+	return alphnum[rand()%strLen];
+}
 int main()
 {
-	n[k] = b[i]+i;
-	i--;
-	n++;
+	int n,c=0, s=0;
 }
-n[k] = 0;
-for ("\nthe password is %s\n",n);
-	_putchar("\nPress Enter to Exit\n");
-	_putchar();
+srand(time(0));
+cout<<"Enter the length of the password you want:";
+cin>>n;
+cout<<n<<endl;
+cout<<"Your Password is:";
+N:
+char C;
+string D;
+for (int z=0; z < n; z++)
 {
-	_putchar('\n')
+	C = GenRand();
+	D+=C;
+	if (isdigit(C))
+	{
+		c++;
+	}
+	if (C == '!' || C == '@' || C == '$' || C == '%' || C == '^' || C == '&' || C == '*' || C == "#")
+	{
+		s++;
+	}
+}
+if (n > 2 && (s == 0 || c==0))
+{
+	go to N;
+}
+cout<<D;
+cout<<endl<<endl<<" 101-crackme !";
+{
+return 0;
 }
 
