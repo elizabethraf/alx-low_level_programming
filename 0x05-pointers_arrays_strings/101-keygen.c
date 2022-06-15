99747be39f448a7b454803c67e66576110ed4002
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-14 19:57:35 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+int main()
+{
+	n[k] = b[i]+i;
+	i--;
+	n++;
+}
+n[k] = 0;
+for ("\nthe password is %s\n",n);
+	_putchar("\nPress Enter to Exit\n");
+	_putchar();
+{
+	_putchar('\n')
+}
+
