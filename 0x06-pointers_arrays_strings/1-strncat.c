7b25f823025b3047e38c81ceb7e5b007ba0285a0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-16 13:14:39 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* *_strncat - Entry point
+* @dest:concatenates two strings
+* @src:concatenates two strings
+* Print with _putchar
+* Return: a pointer resulting string dest
+**/
+char *_strncat(char *dest, char *src, int n)
+{
+	int l = strlen(dest);
+	int i;
+
+for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+	dest[l + i] = src[i];
+	}
+	dest[l + i] = '\0';
+
+	return (dest);
+}
+
