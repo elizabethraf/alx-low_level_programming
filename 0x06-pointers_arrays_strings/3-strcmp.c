970234055ab04f46c4b614944121a1f9355aee00
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-17 17:23:22 -0700

    ./3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,8 @@
 
 /**
 * _strcmp - Entry point
-* @*s1:compare two strings
-* @*s2:compare two strings
+* @s1:compare two strings
+* @s2:compare two strings
 * Print with _putchar
 * Return: same if they are the same or else not the same
 **/
@@ -18,7 +18,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		for (i = 0; s2[i] != '\0'; i++)
 		{
-			if(s1[i] == s2[i])
+			if (s1[i] == s2[i])
 				cnt++;
 		}
 		return (s1[i] - s2[cnt]);
