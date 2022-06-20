e5c445ee900a63f9b5082791b042b7110524900a
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-20 11:15:16 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 2-main.c 2-strchr.c -o 2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* *_strchr - Entry point
+* @s:function that locate a character in a string
+* @c:function that locate a character in a string
+* Print with _putchar
+* Return: a pointer to the memory c and null if the character is not found
+**/
+char *_strchr(char *s, char c)
+{
+
+	char *ptr = s;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+			break;
+
+		s++;
+	}
+	if (s == ptr)
+		return(NULL);
+
+	return (s);
+}
