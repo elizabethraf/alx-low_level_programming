a58a4a5184b3581a64eef3ba31499714c3a6d4bb
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-20 07:19:58 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* *_memset - Entry point
+* @n: function that fills memory
+* Print with _putchar
+* Return: a pointer to the memory area s
+**/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int l;
+
+	for ( l = 0; l < n; n++)
+		*S[l] = b;
+
+  return(s);
+}
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+
+char *_memset(char *s, char b, unsigned int n);
