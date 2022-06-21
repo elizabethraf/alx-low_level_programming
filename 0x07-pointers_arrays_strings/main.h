9b95bf55f93bb3cb1ec3656472f2babe66cf13d2
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-21 09:23:03 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -3,8 +3,6 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
-#include <iostream.h>
-#include <conio.h>
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
