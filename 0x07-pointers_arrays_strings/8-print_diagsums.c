b8a54e691fae4218a520515eac0a02953bbaf94c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-21 10:54:34 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* *_memset - Entry point
+* @s:function that fills memory
+* @b:function that fills memory
+* @n: function that fills memory
+* Print with _putchar
+* Return: a pointer to the memory area s
+**/
+void print_diagsums(int *a, int size)
+{
+	int l;
+	int k;
+	int ttl = 0;
+
+	for (l = 0; l < size; l++)
+	{
+		for (k = 0; k < size; k++)
+			if (l == k)
+				printf("%d ", *(*a+l));
+				//ttl+=**a[l][k];
+		a++;
+	}
+
+	printf("%d \n", ttl);
+
+}
