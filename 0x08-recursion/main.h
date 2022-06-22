9c9a05e2ad338cb343dfe80a10295a55a450a983
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-22 11:17:28 -0700

    4-pow_recursion.c

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,10 +3,13 @@
 #include <string.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <math.h>
 
 void _puts_recursion(char *s);
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
+int _pow_recursion(int x, int y);
+
 
