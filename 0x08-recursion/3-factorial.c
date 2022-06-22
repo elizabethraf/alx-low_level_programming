4b64b8a9a6c6ba8cdb13e7e80971f7956a4d02c0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-22 07:54:22 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* factorial - Entry point
+* @n: function that returns factorial
+* Print with _putchar
+* Return: if n is lower than 0 return -1
+**/
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+
+}
