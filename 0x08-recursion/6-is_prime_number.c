c3a88544f914c2f0ea262840cc960b43332b9c6f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-23 06:28:47 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,7 +6,7 @@
 * Print with _putchar
 * Return: 1 if input integer is prime number and 0 otherwise
 **/
-int isPalRec(int n)
+int is_prime_number(int n)
 {
 	if (n <= 2)
 	return (n == 2) ? (1) : (0);
@@ -16,23 +16,6 @@ int isPalRec(int n)
 	if ((2 * 2) > n)
 		return (1);
 
-	isPalRec(n + 1);
+	is_prime_number(n + 1);
 	return (0);
-}
-
-int is_prime_number(int n)
-{
-	int n = 2;
-
-	if (n == 0)
-		return (1);
-return isPalRec(n - 1);
-}
-
-int main()
-{
-	n = 2;
-
-		if (is_prime_number(n))
-			return (0);
 }
