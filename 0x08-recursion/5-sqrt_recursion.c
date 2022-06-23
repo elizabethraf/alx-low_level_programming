6528c0e7c855610c759d0f2aaa91c454e89acb66
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-22 19:21:54 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,9 +8,13 @@
 **/
 int _sqrt_recursion(int n)
 {
+	if (n <= 2)
+		return (1);
+
 	if ((n% n) != 0)
 		return (-1);
 	else
-		return (n && _sqrt_recursion(n - 1));
+		return _sqrt_recursion(n - 1);
+	n++;
 }
 
