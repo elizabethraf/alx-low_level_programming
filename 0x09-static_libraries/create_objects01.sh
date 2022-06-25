a22f57e146281b96a57297985c574b3b985909d2
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-25 01:55:13 -0700

    create_objects01.sh

--- 0x09-static_libraries/create_objects01.sh
+++ 0x09-static_libraries/create_objects01.sh
@@ -1,3 +1,7 @@
-#!/bin?bash
-ar -rcs libmy.a 0-isupper.o 0-memset.o 0-strcat.o 100-atoi.o 1-memcpy.o 1-strncat.o 2-strch.o 2-strncpy.o 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o 4-isalpha.o 
+#!/bin/bash
+
+for LIBS in $(ls *.o); do
+ar -rcs libmy.a $LIBS;
+done
+
 
