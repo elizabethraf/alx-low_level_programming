a4a8dc236f8628593a9bc7f4cbcfc053c8148e65
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-25 12:26:12 -0700

    whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* main - check the code.
+* @argc:print
+* @argv:print itself
+* Print with _putchar
+* Return: 0 always
+**/
+int main(int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
+}
