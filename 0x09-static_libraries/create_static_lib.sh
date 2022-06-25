9e6d4bb9c22d30fe3fe30762f4aa9a75985e3252
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-25 07:43:01 -0700

    fixedcreate_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+for PROG in $(ls *.c); do
+gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c $PROG -o ${PROG%.*}.o
+done
+
 for LIBS in $(ls *.o); do
-ar -rcs libmy.a $LIBS;
+ar -rcs liball.a $LIBS;
 done
 
 
