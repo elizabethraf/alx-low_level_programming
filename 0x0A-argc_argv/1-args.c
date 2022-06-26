d0afecb010112886c928e273263d66e694319884
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-26 02:46:49 -0700

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,21 +2,13 @@
 
 /**
 * main - check code
-* Return o always
+* @argc:print arguements
+* @argv: print arguements
+* Return: Always 0
 **/
-
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	int count;
-
-     printf("argc = %d\n", argc);
-     printf ("This program was called with\nargv[0]: %s\n",argv[0]);
-
-    if (argc > 1)
-	{
-        for (count = 1; count < argc; count++)
-
-	 printf("argv[%d] = %s\n", count, argv[count]);
-	}
-		return (0);
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
 }
