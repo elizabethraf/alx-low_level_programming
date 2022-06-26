a897b6a84902c68d48f4265eec00d2158cd6159c
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-26 11:03:50 -0700

    4-add.c error fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,20 +9,24 @@
 int main(int argc, char *argv[])
 {
 	int i, ttl;
+	int error;
 
 	ttl = 0;
+	error = 0;
+
 	if (argc > 1)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!(atoi(argv[i])))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
+			if ( atoi(argv[i]) > 0 )
 				ttl += atoi(argv[i]);
+			else
+				error++;
 		}
+	}
+
+	if ( error < 1)
+	{
 		printf("%d\n", ttl);
 		return (0);
 	}
