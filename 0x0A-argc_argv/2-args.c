d66ae4824b86753caea46ff362b273e3ea27fe70
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-26 04:01:04 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,22 +2,22 @@
 
 /**
 * main - Entry point
-* @c:
+* @argc:all arguement printed
+* @argv: arguement printed
 * Print with _putchar
 * Return: Always 0
 **/
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-    int count;
+	int count;
 
-     printf("argc = %d\n", argc);
-     printf ("This program was called with\nargv[0]: %s\n",argv[0]);
+	printf("argc = %d\n", argc);
+	printf("This program was called with\nargv[0]: %s\n", argv[0]);
 
-    if (argc > 1)
+	if (argc > 1)
 	{
-        for (count = 1; count < argc; count++)
-
-	 printf("argv[%d] = %s\n", count, argv[count]);
+	for (count = 1; count < argc; count++)
+	printf("argv[%d] = %s\n", count, argv[count]);
 	}
 	return (0);
 }
