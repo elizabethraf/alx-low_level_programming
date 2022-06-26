10e67b5ffcf99a64f1fcedd68d93b3877351cf2f
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-26 08:46:01 -0700

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* main - check code
+* @argc: print
+* @argv: number
+* Return: always 0
+**/
+int main(int argc, char *argv[])
+{
+
+	(void)argc;
+
+	if (argc > 1)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
