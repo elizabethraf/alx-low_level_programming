a0f4ada37b410fafddab777a44bd609572447fea
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-26 09:10:52 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* main - check code
+* @argc: print
+* @argv: number
+* Return: always 0
+**/
+int main(int argc, char *argv[])
+{
+	int i, ttl;
+
+	ttl = 0;
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			ttl += atoi(argv[i]);
+		}
+		printf("%d\n", ttl);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}
