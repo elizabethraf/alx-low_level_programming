f18a8b921bbafb2297947c22340b0c995f4b6d43
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-28 16:29:46 -0700

    liball.a

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* *create_array - Entry point
+* @c:creates array
+* Print with _putchar
+* Return:null if size = 0
+**/
+char *create_array(unsigned int size, char c)
+{
+
