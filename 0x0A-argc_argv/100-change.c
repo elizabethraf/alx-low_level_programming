01f1c1880b6f12a5e051544843262973412b4766
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-28 16:59:07 -0700

    revertingchanges

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,6 +8,7 @@
 * */
 int main(int argc, char *argv[])
 {
+<<<<<<< HEAD
 	int coins[5] = {25, 10, 5, 2, 1};
 
 	if (argc > 1 )
@@ -21,3 +22,13 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 }
+=======
+	int coins[] =(25, 10, 5, 2, 1);
+	int amount = atoi(argv[1]);
+	
+	if (argc > 1 )
+	{
+		coinNumber = mycombos(&amount, coins, 0);
+		printf("%
+	}
+>>>>>>> parent of 41c6187... 100-change.c cmp
