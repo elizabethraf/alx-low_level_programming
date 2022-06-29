fd5be6140d1f1df99e603d8d5c4810018e7d88a2
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-28 18:49:46 -0700

    task0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,4 +8,18 @@
 **/
 char *create_array(unsigned int size, char c)
 {
+	unsigned int i;
+	char k = NULL;
 
+	if (len == 0)
+		return (NULL);
+	if (len != 0)
+	{
+		array = (c *(malloc(len * sizeof(c));
+		if (array != NULL)
+
+			for (i = 0; i < len; i++);
+			 array[1] = c;
+		}
+		return (array);
+}
