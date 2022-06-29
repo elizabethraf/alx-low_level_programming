5b2cd14b49f2774cda09a9020914d41365686fb4
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-29 07:16:32 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*  free_grid - Entry point
+* @grid:free a dimensional grid
+* @height:grid function
+* Print with _putchar
+* Return: always 0
+**/
+void free_grid(int **grid, int height);
+{
+	int i;
+	for (i = 0; i < hght; ++i)
+			free(grid[i]);
+	{
+	 free(grid);
+	}
+}
+
+
