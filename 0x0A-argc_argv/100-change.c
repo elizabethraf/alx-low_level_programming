d1b9bc4b2775fd2b386864a1013b5229a5c085f1
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-29 15:56:17 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
 * *main - check code
 * @argv:arguements
 * @argc: size of length
-* Return: 0 always
-* */
+* Return: 1 always
+**/
 int main(int argc, char *argv[])
 {
 	int c[5] = {25, 10, 5, 2, 1};
@@ -13,54 +12,29 @@ int main(int argc, char *argv[])
 	int ttl = 0;
 	int a;
 
-	if (argc > 1 )
-    {
-		a = atoi(argv[1]);
-		if (a < 0)
-		{
-			printf("0\n");
-			return (1);
-		}
-
-        while (i < 5)
-        {
-            if (c[i] <= a)
-            {
-                num = a / c[i];
-                if (c[i] == c[0])
-                {
-                    ttl+=num;
-                }
-                if (c[i] == c[1])
-                {
-                    ttl+=num;
-                }
-                if (c[i] == c[2])
-                {
-                    ttl+=num;
-                }
-                if (c[i] == c[3])
-                {
-                    ttl+=num;
-                }
-                if (c[i] == c[4])
-                {
-                    ttl+=num;
-                }
-
-                a = a - num * c[i];
-            }
-
-            i++;
-        }
-        printf("%d\n", ttl);
-        return (0);
-    }
-    else
-    {
-        printf("Error\n");
-        return (1);
-    }
-
+	if (argc > 1)
+	{
+	a = atoi(argv[1]);
+	if (a < 0)
+	printf("0\n");
+		return (1);
+			if (c[i] <= a)
+			num = a / c[i];
+			if (c[i] == c[0];
+				ttl += num;
+			if (c[i] == c[1])
+				ttl += num;
+				if (c[i] == c[2])
+				ttl += num;
+				if (c[i] == c[3])
+				ttl += num;
+				if (c[i] == c[4])
+				ttl += num;
+				a = a - num * c[i];
+				i++;
+		printf("%d\n", ttl);
+		return (0);
+	}
+		printf("Error\n");
+		return (1);
 }
-
