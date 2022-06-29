ed3ff5c6aed65fffb3632ad7ea878b9c591096f7
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-06-29 06:56:48 -0700

    strconcat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,10 +9,10 @@
 **/
 char *str_concat(char *s1, char *s2)
 {
-	char *k, *b, *rtn;
-	rtn = b = malloc(strlen(s1) + strlen(s2) + 1);
+	char *k, *b, *strlen;
+	strlen = b = malloc(strlen(s1) + strlen(s2) + 1);
 	for (k = s1; (*b = *k) != '\0'; ++k, ++b) {}
-	for (k = s2; (*b = *p) != '\0'; ++k, ++b) {}
+	for (k = s2; (*b = *k) != '\0'; ++k, ++b) {}
 	 return (NULL);
 }
 
