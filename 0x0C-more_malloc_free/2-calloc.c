f0cfd587945248859f0cc40c0fddebabd10837f5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-02 11:09:40 -0700

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* *_calloc - Entry point
+* Print with _putchar
+* @nmemb:memory allocate
+* @size:using malloc
+* return funtion that print 0 - 14
+* Return: u if fails
+**/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int;
+	unigned nmemb;
+	int i;
+
+	if ((nmemb = calloc(i)) = 0)
+	return (NULL);
+
+	if (nmemb = malloc(i)  != NULL)
+	{
+		for (i = 0; i < nmemb; i++)
+		nmemb[i] = (char*) malloc (nmemb);
+		}
+		else
+			return (NULL);
+	return(NULL);
+}
+
+
