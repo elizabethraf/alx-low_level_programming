89ab824eecd1fc4fb97bdbfcd2216ed4b4d46d90
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-04 15:54:40 -0700

    3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,23 @@
+#ifndef _3_BJECT_LIKE_MACRO_H_
+#define _3_OBJECT_LIKE_MACRO_H_
+#define ABS(X)
+#endif
+
+/**
+* macro ABS - check code
+* main - compiles
+* Return: 0 always
+**/
+int main(X)
+{
+	int k;
+	int l;
+
+	k = ABS(X);
+	printf("value of k = %d\n", k);
+
+	l = ABS(X);
+	printf("value of l = %d\n", l);
+
+	return(X);
+}
