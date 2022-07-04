bcc43e39c14fec4bd5b923378726cacef67d4258
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-04 11:34:34 -0700

    macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,12 +1,4 @@
 #include <stdio.h>
-#define BEGIN {
-#define END}
-#define Interger int
 
-#define SIZE(1024)
-main()
-{
-	size = 1024
-		return (0);
-}
-
+#define BUFFER_SIZE 1024
+foo = (char*) malloc (BUFFER_SIZE);
