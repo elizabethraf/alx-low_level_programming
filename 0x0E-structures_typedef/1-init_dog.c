84dea00a6e9976c7659592c8005bb8984141b2d4
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-05 09:48:44 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,8 @@
 **/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	struct dog *p = malloc(sizeof(d));
-
-	p->name = name;
-	p->age = age;
-	p->owner = owner;
-	d = p;
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
 
