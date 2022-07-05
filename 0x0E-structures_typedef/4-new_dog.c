083b4952d598b69944aec1b62d88cea5deb58fd6
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-05 14:51:52 -0700

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+* *new_dog - check code
+* @name:
+* @age:
+* @owner:
+* Return: NULL if function fails
+**/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	new_dog temp;
+	temp.name = name;
+	temp.age = age;
+	temp.owner;
+
+return NULL;
+}
