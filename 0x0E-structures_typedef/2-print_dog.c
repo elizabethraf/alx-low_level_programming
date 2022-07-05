35770dc4ce8d337a4c3cdfa9c10bb9e178270a72
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-05 13:44:34 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,7 +13,7 @@ void print_dog(struct dog *d)
 	}
 	if (d->name == NULL || d->age == 0 || d->owner == NULL)
 	{
-		printf("(nil)");
+		d.name ? printf("Name: (nil)") : printf("Owner: (nil)");
 	}
 	else
 	{
