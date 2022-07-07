b6cac542ab0d485cb96483c026158ad31ee1263d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-07 16:31:40 -0700

    3-op_functions.c args

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,15 +9,19 @@
 int main(int argc, char *argv[])
 {
 	int argcount = argc;
-	char *operto = argv[2];
-	int nb1 = atoi(argv[1]);
-	int nb2 = atoi(argv[3]);
+	int nb1, nb2;
+	char *operto;
+
 
 	if (argcount < 4 || argcount > 4)
 	{
 		ERP;
 		exit(98);
 	}
+
+	operto = argv[2];
+	nb1 = atoi(argv[1]);
+	nb2 = atoi(argv[3]);
 
 	if ((*operto == '%' && nb2 == 0)  || (*operto == '/' && nb1 == 0))
 	{
