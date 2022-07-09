2c3c70dbaeb64b331cc3dcf60132026ec7522269
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-09 12:20:18 -0700

    2-int_index.c nullp

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,7 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	 int k;
 	 int ind;
 
-    if (size <= 0 || size == NULL)
+    if (size <= 0 || cmp == NULL)
 		return (-1);
 
 	if (array == NULL)
