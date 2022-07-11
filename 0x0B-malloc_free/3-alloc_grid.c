484f56ab10a8cbf4e03a12062a54680ed7e9bc4e
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-10 17:53:31 -0700

    3-fixed

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,8 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	s = malloc(height * sizeof(int *)
+
+	s = malloc(height * sizeof(int *))
 	if (s == NULL)
 	{
 		free(s);
