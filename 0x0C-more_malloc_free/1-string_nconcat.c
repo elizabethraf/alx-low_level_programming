7e7249352c35e784e2a38ba94f6d8a3f89946bad
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-11 14:35:29 -0700

    1-string_nconcat.c null

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,8 +10,8 @@
 **/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int s1_len = strlen(s1);
-	unsigned int s2_len = strlen(s2);
+	unsigned int s1_len;
+	unsigned int s2_len;
 	char *s;
 	unsigned int i, len, short_len2;
 
@@ -19,6 +19,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
 	if (n >= s2_len)
 	{
 		s = malloc((s1_len + s2_len + 1) * sizeof(*s1));
