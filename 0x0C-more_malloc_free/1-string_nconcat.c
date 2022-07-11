8a2f515666b3c5d572f0e937a3218a5c87fe9549
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-11 14:26:44 -0700

    1-string_nconcat.c shortlen

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,28 +13,33 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int s1_len = strlen(s1);
 	unsigned int s2_len = strlen(s2);
 	char *s;
-	unsigned int i, len;
+	unsigned int i, len, short_len2;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	if (n >= s2_len)
 	{
 		s = malloc((s1_len + s2_len + 1) * sizeof(*s1));
-		len = ((s1_len + s2_len + 1) * sizeof(*s1));
+		short_len2 = ((s1_len + s2_len + 1) * sizeof(*s1));
 	}
 	else
 	{
 		s = malloc((strlen(s1) + n + 1) * sizeof(*s1));
-		len = ((strlen(s1) + n + 1) * sizeof(*s1));
+		short_len2 = ((n + 1) * sizeof(*s1));
 	}
 
 	if (s == NULL)
 		return (NULL);
-
+	len = 0;
 	for (i = 0; i < s1_len; i++)
-		s[i] = s1[i];
-	for (i = 0; i < s2_len; i++)
-		s[s1_len + i] = s2[i];
+		s[len++] = s1[i];
 
-	s[len - 1] = '\0';
+	for (i = 0; i < short_len2; i++)
+		s[len++] = s2[i];
+
+	s[len] = '\0';
 
 	return (s);
 }
