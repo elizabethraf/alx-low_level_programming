ef49ab3a4cf6ae2653408609af2846351ee4e75d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-14 09:36:20 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,6 +15,9 @@ size_t print_list(const list_t *h)
 		return (0);
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+
 		printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		len++;
