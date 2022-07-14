cab0435a614bb3fc2e1d418a62f5ff3fc923a1df
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-14 11:07:55 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 
-void beforemain()
+void beforemain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
