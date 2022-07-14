a4a5b898e260104bc1fdcd3853b5a74e529eda91
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-14 03:31:22 -0700

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+* print_list - check code
+* @h: print elements
+* Return: the number of nodes
+*
+**/
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	int str;
+
+	while ( h != NULL)
+	{
+		if (h == NULL | str == NULL)
+			return (NULL);
+			printf("[0] (nil)");
+	{
+		printf ("[%d]", const *);
+		printf ("[%s]", const *);
+	}
+	h = h;
+	count++;
+	printf("\n");
+	}
+	return (count);
+}
+
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,10 @@
+#ifndef _LIST_H
+#define _LIST_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+size_t print_list(const list_t *h);
+
+#endif
