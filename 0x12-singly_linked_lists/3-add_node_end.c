27d1c0cdcc6a8d71b38ccae24270affa473eae07
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-14 07:04:29 -0700

    3-add_node_end.c

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_node_end - check code
+ * @head: pointer to first node in linked list
+ * @str: copy data to string of the node
+ * Return: the address  or NULL if it failed
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *tail;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		tail = *head;
+		while (1)
+		{
+			if (tail->next == NULL)
+			{
+				tail->next = new;
+				break;
+				}
+			tail = tail->next;
+		}
+	}
+	return (0);
+}
