42bda03bc83665f4b754404f31b7efed57886286
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-14 07:07:36 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,11 +12,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *tail;
 
 	new = malloc(sizeof(list_t));
+
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
 	new->len = strlen(str);
 	new->next = NULL;
+
 	if (*head == NULL)
 		*head = new;
 	else
@@ -32,5 +35,5 @@ list_t *add_node_end(list_t **head, const char *str)
 			tail = tail->next;
 		}
 	}
-	return (0);
+	return (new);
 }
