487f79c807bf688bdbdf5461aa2dceb318256bac
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-17 14:13:26 -0700

    2-large

---
+++ 0x03-debugging/2-main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include "main.h"
+
+int largest_number(int a, int b, int c)
+{
+	a = 972;
+	b = -98;
+	c = 0;
+    int largest;
+
+    if (a > b && a > c)
+    {
+        largest = a;
+    }
+    else if (b > a && b > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
+int main(void)
+{
+		int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}
+
+int main(void)
+{
+        int a, b, c;
+        int largest;
+
+        a = 0;
+        b = -0;
+        c = 0;
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
+}
