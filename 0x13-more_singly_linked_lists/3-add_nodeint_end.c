1a329f09cf3a5ab255f02c593a6d95ee57371db5
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-18 16:52:30 -0700

    3-add_nodeint_end.c

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+* *add_nodeint_end - check code
+* @n: add new nodes at the end
+* @head: adding list at the end
+* Return: address or NULL if it failed
+**/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	listint_t *current_node;
+
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	if (!*head)
+		*head = new_node;
+	else
+	{
+		current_node = *head;
+	while (current_node->next)
+		current_node = current_node->next;
+
+		current_node->next = new_node;
+	}
+	return (*head);
+}
+
