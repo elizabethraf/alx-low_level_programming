806d574838c02a2fc6f3c21883663f84769adaa6
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-19 12:10:36 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,6 +9,7 @@
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node, *current_node;
+
 	current_node = *head;
 
 	new_node = malloc(sizeof(listint_t));
@@ -18,14 +19,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 	if (*head == NULL)
-		*head = new_node;
-	else
 	{
-		while (current_node->next != NULL)
-		{
-			current_node = current_node->next;
-		}
-		current_node->next = new_node;
+		*head = new_node;
+		return (new_node);
 	}
+
+	while (current_node->next != NULL)
+		current_node = current_node->next;
+
+	current_node->next = new_node;
 	return (new_node);
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+* *insert_nodeint_at_index - check code
+* @n: insert new node
+* @idx:insert new node
+* @head: given position
+* Return: address of new node or NULL if failed
+**/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t 
