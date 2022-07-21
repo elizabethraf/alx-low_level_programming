5a3c3ae6dd1a85c62761c97edf77099a25caf06d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-21 15:02:59 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+* print_binary - check the code
+* @n: prints binary representation
+**/
+void print_binary(unsigned long int n)
+{
+	unsigned int k, l, lmask;
+	char start_p = 'f';
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+
+	for (k = 32; k > 0; k--)
+	{
+		l = n >> (k - 1);
+		lmask = l & 1;
+
+		if (lmask == 1)
+			start_p = 't';
+
+		if (start_p == 't')
+			_putchar('0' + lmask);
+	}
+}
