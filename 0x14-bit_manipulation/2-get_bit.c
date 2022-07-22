1112bcd4b508e05a0feb445375b811e510cbe9f9
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-22 02:44:32 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* get_bit - check code
+* @n: function that returns the value of bit
+* @index: value of a bit given index
+* Return: value of bit index or -1 if error occured
+**/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int len = n;
+
+	if (n == 0 || index == 0)
+		return (0);
+
+	if (!(len & (1 << index)))
+	{
+		len = len | (1 << index);
+			return (1);
+	}
+	else
+	{
+		return (1);
+	}
+}
