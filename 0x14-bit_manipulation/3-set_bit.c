2798c770dc5f1d93189095a6422ce1d4961dfb1b
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-22 02:53:50 -0700

    3-set_bit.c

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* set_bit - check code
+* @n: sets value of bits
+* @index: value of bit at given index
+* Return: 1 if it worked or -1 if error occured
+**/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (!(*n & (1 << index)))
+	{
+		*n = *n | (1 << index);
+			return (1);
+	}
+	return (1);
+}
