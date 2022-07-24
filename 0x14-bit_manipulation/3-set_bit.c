aee27eb2f24fa941b96931e34192a9c7d67e7e5d
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-24 14:06:22 -0700

    3-st

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,9 +10,9 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (!(*n & (1 << index)))
 		return (-1);
-	{
-		*n = *n | (1 << index);
-			return (1);
-	}
+
+	*n = *n | (1 << index);
+		return (1);
+
 	return (1);
 }
