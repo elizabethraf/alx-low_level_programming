dbe5a74b666852c48a46ee9f2aff4656f2a9e6ea
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-26 10:55:18 -0700

    1-create_file.c-empty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-		return (-1);
+		*text_content = "";
 
 	a = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (a == -1)
