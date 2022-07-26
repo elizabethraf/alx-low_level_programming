388cebdf497252534cf30e2930b43570b3f79c77
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-26 03:26:07 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,13 +14,12 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (text_content == NULL)
-
-	a = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (a == -1)
 		return (-1);
 
-	for (len = 0; text_content[len])
-		len++;
+	a = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (a == -1)
+		return (-1);
+	for (len = 0; text_content[len]; len++);
 	b = write(a, text_content, strlen(text_content));
 	if (b > 0)
 		return (-1);
