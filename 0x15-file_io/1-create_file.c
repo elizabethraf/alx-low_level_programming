1e254c41a3d14437504a99a2ef32c14aecaede2a
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-26 10:51:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	for (len = 0; text_content[len]; len++);
 	b = write(a, text_content, strlen(text_content));
 	if (b > 0)
-		return (-1);
+		return (1);
 
 	close(a);
 	return (1);
