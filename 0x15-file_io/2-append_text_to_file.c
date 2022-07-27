258317c2448b31b78b4818259b556255ba598b72
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-27 14:30:09 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,17 +13,20 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content == NULL)
+		text_content = "";
+
+
+	if ((pr = open(filename, O_WRONLY | O_CREAT| O_EXCL, S_IRUSR | S_IWUSR)) == 17)
 		return (1);
 
-	pr = open(filename, O_WRONLY | O_APPEND, S_IRUSR | S_IWUSR);
-	if (pr == -1)
-		return (-1);
-
 	len = write(pr, text_content, strlen(text_content));
-
-	if (len == -1)
+	if (len == -1 || len == 13)
+	{
+		close (pr);
 		return (-1);
+	}
 
 	close(pr);
 	return (1);
 }
+
