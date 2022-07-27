33d230f015b6bdfc16fc13a0e96e81e168b6b072
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-27 05:16:06 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,20 +1,18 @@
+#include <stdio.h>
+
 /**
 * main - Entry point
 * check is the rand n is negative or positive,
 * or zero and print accordingly
 * Return: Always (0) (Success)
-**/
+*/
+int main (void)
+{
+	int n;
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d\n", n);
+	}
 
-#include <stdio.h>
- 
-int main (void) {
-
-   int a = 10;
-
-   while( a < 20 ) {
-      printf("value of a: %d\n", a);
-      a++;
-      printf('\n');
-   }
- 
-   return (0);
+	return (0);
+}
