8da29af190a14e2af5d43fb9e5d5931d97d9baa0
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-27 06:41:55 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+* check is the rand n is negative or positive,
+* or zero and print accordingly
+* Return: Always (0) (Success)
+**/
+int main(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar(10);
+
+	return (0);
+}
