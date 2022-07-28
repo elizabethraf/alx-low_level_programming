23e2c1e5c2b198c201033e1112c3a4b88c84bf37
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-28 01:22:43 -0700

    102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+/**
+* main - Entry point
+* different combinations of three digits.
+* Return: Always (0) (Success)
+**/
+int main(void)
+{
+	int ch;
+	int a;
+	int b;
+	int c;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (a = 48; a <= 57; a++)
+		{
+			for (b = 48; b <= 57; b++)
+			{
+				for (c = 49; c <= 57; c++)
+				{
+					putchar(ch);
+					putchar(a);
+					putchar(32);
+					if (c == b)
+					   b++;	
+					putchar(b);
+
+					if (c > ch)
+						c++;
+
+					putchar(c);
+
+					if (ch < 56)
+					{
+						putchar(44);
+						putchar(32);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
