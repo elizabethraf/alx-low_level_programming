9811683e737e3fce448bcf25b21763a9724006e6
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-28 02:52:14 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (ch = 48; ch <= 57; ch++)
 	{
-		for (a = 48; a <= 57; a++)
-		{
-				putchar(ch);
-				putchar(a);
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(ch);
+		putchar(a);
 
-				if (ch != 57 || a != 57)
-				{
-					putchar(44);
-					putchar(32);
-				}
-			}
+		if (ch != 57 || a != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
 	}
 	putchar('\n');
 
