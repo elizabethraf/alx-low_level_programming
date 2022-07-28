5e64e9c1f30f69b5addc64025ab6341216abadf8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-28 03:43:47 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,16 +11,6 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-			break;
-		}
-
-		s1++;
-		s2++;
-	}
-
-	return (*s1 - *s2);
+	i = strcmp(s1, s2);
+	return (i);
 }
