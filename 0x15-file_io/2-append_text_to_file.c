f422f4463c7ecceeb0160f6e12eb9a51be4e6b71
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-29 11:08:03 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* append_text_to_file - check point
-* @filename: function that appends text
+* append_text_to_file - function that appends text
+* @filename: check point file
 * @text_content: appends text
 * Return: 1 on success and -1 on failure
 **/
@@ -12,17 +12,22 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
+
 	if (text_content == NULL)
 		text_content = "";
 
+	pr = open(filename, O_WRONLY | O_APPEND);
 
-	if ((pr = open(filename, O_WRONLY | O_CREAT| O_EXCL, S_IRUSR | S_IWUSR)) == 17)
-		return (1);
+	if (pr == 2)
+	{
+		close(pr);
+		return (-1);
+	}
 
 	len = write(pr, text_content, strlen(text_content));
 	if (len == -1 || len == 13)
 	{
-		close (pr);
+		close(pr);
 		return (-1);
 	}
 
