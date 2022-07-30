ba0b8c9f25f7ad811b9587a6e78b722f405c2071
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-07-30 14:56:18 -0700

    3-cp.c bug fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-int read_cpfile(const char *filename, const char *fileto)
+int readwrite_cpfile(const char *filename, const char *fileto)
 {
-	int file_from, file_to, reading, writing;
+	int file_from, file_to, reading, b;
 	char *letters;
-	int len;
+	int len = 1024;
 
 	file_from = open(filename, O_RDONLY);
 
@@ -14,17 +14,11 @@ int read_cpfile(const char *filename, const char *fileto)
 		exit (98);
 	}
 
-	len = 0;
-	while (fgetc(file_from) !=EOF )
-	{
-		len++;
-	}
-
 	letters = malloc(sizeof(char) * len + 1);
 	if (letters == NULL)
 		exit (98);
 
-	reading = read(filename, letters, len);
+	reading = read(file_from, letters, len);
 	if (reading == -1)
 	{
 		free(letters);
@@ -41,9 +35,18 @@ int read_cpfile(const char *filename, const char *fileto)
 		exit (99);
 	}
 
+	b = write(file_to, letters, len);
+
+	free(letters);
+	close(file_from);
+	close(file_to);
+
+	if (b > 0)
+		return (b);
+	else 
+		return (98);
+
 }
-
-
 
 
 
@@ -51,7 +54,7 @@ int read_cpfile(const char *filename, const char *fileto)
 * 
 *
 **/
-int main(int argc, char *argv)
+int main(int argc, const char **argv)
 {
 
     if (argc != 3)
@@ -60,72 +63,7 @@ int main(int argc, char *argv)
         exit(97);
     }
 
+	readwrite_cpfile(argv[1], argv[2]);
 
-
-	if (argc == NULL || argv == 0)
-		return (0);
-
-	reading = readwrite_cpfile(argv[1], argv[2]);
-	
-	len = malloc(sizeof(char) * letters + 1);
-	if (len == NULL)
-		return (0);
-
-
-	if (reading == -1)
-	{
-		free(len);
-		close(file);
-		return (0);
-	}
-
-	writing = write(STDOUT_FILENO, len, reading);
-	if (writing == -1)
-	{
-		free(len);
-		close(file);
-		return (0);
-	}
-
-	free(len);
-	close(file);
-	return (writing);
+	return (0);
 }
-
-#include "main.h"
-
-/**
-* append_text_to_file - function that appends text
-* @filename: check point file
-* @text_content: appends text
-* Return: 1 on success and -1 on failure
-**/
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int pr, len;
-
-	if (filename == NULL)
-		return (-1);
-
-	if (text_content == NULL)
-		text_content = "";
-
-	pr = open(filename, O_WRONLY | O_APPEND);
-
-	if (pr == 2)
-	{
-		close(pr);
-		return (-1);
-	}
-
-	len = write(pr, text_content, strlen(text_content));
-	if (len == -1 || len == 13)
-	{
-		close(pr);
-		return (-1);
-	}
-
-	close(pr);
-	return (1);
-}
-
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,8 +11,6 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-read_cpfile(const char *filename);
-create_cpfile(const char *filename);
-
+int readwrite_cpfile(const char *filename, const char *fileto);
 #endif
 
