e6f4f8a388d171dfd946575e3f71ae816c3bb304
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-01 12:09:44 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,14 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t b;
 
-	if (b == 0 || h != NULL)
-		b++;
+	b = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		b++;
 	}
 	return (b);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,5 +1,15 @@
+#ifndef _LISTS_H
+#define _LISTS_H
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+
+typedef struct dlistint_t {
+    int n;
+    struct dlistint_t *next;
+    struct dlistint_t *prev;
+} dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
+
+#endif
