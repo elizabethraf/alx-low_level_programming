fc01322b28645482fc100a86e22ea0370e7228e8
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-09-28 10:57:57 -0700

    3-hash_table_set.c and 4-hash_table_get.c

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_get - check code
+* @key: retrieve a value associated with a key
+* Return: the value or NULL if key couldn't be found
+**/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = 0;
+	hash_node_t *len;
+
+	if (key == NULL || ht == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, (*ht).size);
+	len = (*ht).array[index];
+	while (len != NULL)
+	{
+		if (strcmp(len->key, key) == 0)
+			return (len->value);
+		len = len->next;
+	}
+	return (NULL);
+}
