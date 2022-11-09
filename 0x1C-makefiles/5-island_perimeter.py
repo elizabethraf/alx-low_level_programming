468089aa188c2ab423730275d81b43f2a9658038
elizabethraf <elizabethraf@matamaneholdings.co.za>
2022-11-09 13:38:55 -0800

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Display that returns the perimeter of the island described in grid
+
+"""
+
+
+def island_perimeter(grid):
+    """Display that returns the perimeter of the island described in grid"""
+    b = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
+
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    b += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    b += 1
+                if j == width or grid[i][j + 1] != 1:
+                    b += 1
+                if i == length or grid[i + 1][j] != 1:
+                    b += 1
+    return  b
